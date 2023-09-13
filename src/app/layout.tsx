d2fc793e523a97094596d2ce6b46658cbfe27031
Aleksei Lapygin <alex.lapygin@gmail.com>
2023-09-13 14:15:12 +0200

    feat: changed font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,22 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import './globals.css';
+import type { Metadata } from 'next';
+import { Open_Sans } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] })
+const openSans = Open_Sans({ subsets: ['latin', 'cyrillic'] });
 
 export const metadata: Metadata = {
   title: 'Space Rock Blocker',
   description: 'Block unwanted space rocks',
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={openSans.className}>{children}</body>
     </html>
-  )
+  );
 }

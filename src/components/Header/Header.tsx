621642916b91313f4a47db5f9992b215caf1362b
Aleksei Lapygin <alex.lapygin@gmail.com>
2023-09-15 00:13:06 +0200

    feat: header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,20 @@
+import classNames from 'classnames';
+import { useTranslation } from '@/app/i18n';
+import { passionOne } from '@/app/[lng]/fonts';
+import styles from './Header.module.css';
+import Link from 'next/link';
+
+export default async function Header({ lng }: { lng: string }) {
+  const { t } = await useTranslation(lng, 'Header');
+
+  return (
+    <header className={styles.container}>
+      <Link href={'/'}>
+        <h1 className={classNames(styles.title, passionOne.className)}>
+          {t('title')}
+        </h1>
+      </Link>
+      <div className={styles.subtitle}>{t('subtitle')}</div>
+    </header>
+  );
+}

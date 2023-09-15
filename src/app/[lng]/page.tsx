0365531999cc84ba9f8a5f4d9f0438c243e6bf2b
Aleksei Lapygin <alex.lapygin@gmail.com>
2023-09-15 13:24:40 +0200

    feat: earth image

--- src/app/[lng]/page.tsx
+++ src/app/[lng]/page.tsx
@@ -1,8 +1,10 @@
+import Image from 'next/image';
 import { useTranslation } from '@/app/i18n';
 import AsteroidEntryGroup from '@/components/AsteroidEntryGroup/AsteroidEntryGroup';
 import UnitsSwitcher from '@/components/UnitsSwither/UnitsSwitcher';
 import DistanceUnitsProvider from '@/contexts/distanceUnitsContext';
 import { fetchtAsteroidsFeed } from '@/services/api';
+import earth from '../../../public/earth.webp';
 import styles from './page.module.css';
 
 export default async function Home({
@@ -14,8 +16,9 @@ export default async function Home({
   const { t } = await useTranslation(lng, 'translation');
 
   return (
-    <main>
-      <div className={styles.container}>
+    <main className={styles.main}>
+      <Image src={earth} alt="Earth from space" className={styles.earth} />
+      <div className={styles.feedContainer}>
         <DistanceUnitsProvider>
           <div className={styles.headerContainer}>
             <h1 className={styles.header}>{t('title')}</h1>
